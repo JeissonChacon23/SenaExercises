6e2f55d4e4fb11e5466618665021f4d3627db883
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-29 18:31:29 -0500

    Ejercicio 10

--- Taller.c
+++ Taller.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<math.h>
+
 int main() {
-         //Construya un df que resuelva el problema que tienen en una gasolinera los surtidores de la misma registran lo que surten en galones, pero el precio de la gasolina está fijado en litros. El df debe calcular e imprimir lo que hay que cobrarle al cliente.
-            float galones = 3.5;
-            float precioGalonGasolina = 13660;
-            float precioCliente = galones * precioGalonGasolina;
-            printf("Valor a Pagar: %f",precioCliente);
+         //Diseñe un diagrama de flujo tal que dado como datos el nombre de un dinosaurios Su Peso y su longitud, expresados estos dos últimos en libras y pies respectivamente’ escriba el nombre del dinosaurio, Su Peso expresado en kilogramos y su longitud expresada en metros (l tonelada equivale a lOOO kilogramos y un 1 pie equivale a O,3047 metros).
+            char nombre[30] = "Mastodonte";
+            float peso = 3200; //libras
+            float longitud = 20; //pies
+            float longitudMetros = longitud * 0.3048;
+            float pesoKilo = peso * 0.453592;
+            float pesoTonelada = peso * 0.000453592;
+            printf("Nombre: %s\nLongitud Pies: %f\nLongitud Metros: %f\nPeso Libras: %f\nPeso Kilos: %f\nPeso Toneladas: %f",nombre, longitud, longitudMetros, peso, pesoKilo, pesoTonelada);
          }
