99cba09e7b647470c95aa3de169e15e0f260d3f8
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-30 14:06:57 -0500

    Ejercicio 16

--- Taller.c
+++ Taller.c
@@ -3,14 +3,36 @@
 #include<math.h>
 
 int main() {
-         //Tres personas deciden invertir su dinero para crear una empresa. cada una de ellas invierte una Cantidad distinta. 
-         //Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida.
-            double persona1 = 10000000;
-            double persona2 = 8000000;
-            double persona3 = 11000000;
-            double cantidadTotal = persona1 + persona2 + persona3;
-            double porcentaje1 = (100 * persona1) / cantidadTotal;
-            double porcentaje2 = (100 * persona2) / cantidadTotal;
-            double porcentaje3 = (100 * persona3) / cantidadTotal;
-            printf("Persona 1: %f\nPersona 2: %f\nPersona 3: %f", porcentaje1, porcentaje2, porcentaje3);
+         // En una empresa que fabrica 3 tipos de productos (A, B, C) se cuenta con la colaboración de 3 empleados (E1, E2, E3) 
+         //para la evaluaci6n de los mismos. Los tiempos que cada empleado demora en la elaboración de un artículo de cada tipo de producto 
+         //se dan en la siguiente tabla en minutos:
+            int empleado = 1;
+            int productoA;
+            int productoB;
+            int productoC;
+            int tiempo;
+
+            if(empleado == 1){
+                productoA = 12;
+                productoB = 13;
+                productoC = 7;
+                tiempo = productoA + productoB + productoC;
+                printf("Al empleado %d le toma %d minutos la elaboracion de los articulos\n",empleado, tiempo);
+            }
+            empleado = 2;
+            if(empleado == 2){
+                productoA = 9;
+                productoB = 15;
+                productoC = 17;
+                tiempo = productoA + productoB + productoC;
+                printf("Al empleado %d le toma %d minutos la elaboracion de los articulos\n",empleado, tiempo);
+            }
+            empleado =3;
+            if(empleado == 3){
+                productoA = 15;
+                productoB = 8;
+                productoC = 6;
+                tiempo = productoA + productoB + productoC;
+                printf("Al empleado %d le toma %d minutos la elaboracion de los articulos\n",empleado, tiempo);
+            }
          }
