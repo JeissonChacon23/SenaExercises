24894aabd953b24da403fb29dd94bc0e0c4e0d59
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-30 12:01:51 -0500

    Ejercicio 12

--- Taller.c
+++ Taller.c
@@ -3,13 +3,24 @@
 #include<math.h>
 
 int main() {
-         // Construya un diagrama de flujo que resuelva el problema que tiene una gasolinera. 
-         //Los surtidores de la misma registran lo que surten, en galones・Pero el precio de la gasolina está fijado en litros. 
-         //El diagrama debe calcular e imprimir lo que hay que cobrarle al cliente. (1 galón equivale a 3,785 1itros).
-           
-            float galones = 3.5;
-            float litros = galones * 3.785;
-            float precio = 13660 / 3.785;
-            float valorPagar = litros * precio;
-            printf("Valor a pagar: %f",valorPagar);
+         //construya un diagrama de flujo que calcule la distancia entre dos puntos. Dado como datos las coordenadas de los puntos Pl y P2.
+            int distancia;
+            int p1[2];
+            p1[0] = 2;
+            p1[1] = 2;
+            int p2[2];
+            p2[0] = 2;
+            p2[1] = 4;
+            distancia = sqrt(((p2[0] - p1[0]) * 2 ) + ((p2[1] - p1[1]) * 2));
+            printf("Distancia %d",distancia);
+
+
+
+            int medida[5];
+            medida[0] = 2;
+            medida[1] = 5;
+            medida[2] = 3;
+            medida[3] = 6;
+            medida[4] = 7;
+            //medida[5] = 10; Esta no la acepta ya que se sale del tama;o asignado del vector 
          }
