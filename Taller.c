a35ef75d53de2d219a111ff3c07dccb460baf76a
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-29 22:45:47 -0500

    Ejercicio 11

--- Taller.c
+++ Taller.c
@@ -3,12 +3,13 @@
 #include<math.h>
 
 int main() {
-         //Diseñe un diagrama de flujo tal que dado como datos el nombre de un dinosaurios Su Peso y su longitud, expresados estos dos últimos en libras y pies respectivamente’ escriba el nombre del dinosaurio, Su Peso expresado en kilogramos y su longitud expresada en metros (l tonelada equivale a lOOO kilogramos y un 1 pie equivale a O,3047 metros).
-            char nombre[30] = "Mastodonte";
-            float peso = 3200; //libras
-            float longitud = 20; //pies
-            float longitudMetros = longitud * 0.3048;
-            float pesoKilo = peso * 0.453592;
-            float pesoTonelada = peso * 0.000453592;
-            printf("Nombre: %s\nLongitud Pies: %f\nLongitud Metros: %f\nPeso Libras: %f\nPeso Kilos: %f\nPeso Toneladas: %f",nombre, longitud, longitudMetros, peso, pesoKilo, pesoTonelada);
+         // Construya un diagrama de flujo que resuelva el problema que tiene una gasolinera. 
+         //Los surtidores de la misma registran lo que surten, en galones・Pero el precio de la gasolina está fijado en litros. 
+         //El diagrama debe calcular e imprimir lo que hay que cobrarle al cliente. (1 galón equivale a 3,785 1itros).
+           
+            float galones = 3.5;
+            float litros = galones * 3.785;
+            float precio = 13660 / 3.785;
+            float valorPagar = litros * precio;
+            printf("Valor a pagar: %f",valorPagar);
          }
