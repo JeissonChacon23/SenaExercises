a20a0b9a73c022e7118e54c96a31457469b97b9b
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-30 12:34:17 -0500

    Ejercicio 15

--- Taller.c
+++ Taller.c
@@ -3,13 +3,14 @@
 #include<math.h>
 
 int main() {
-         //Dada la estabilidad económica que existe en un país de Latinoam6rica, las agencias automotrices comienzan a ofrecer 
-         //distintos planes de financiamiento para la comercializaci6n de sus vehículos. 
-         //La empresa XGW ofrece el siguiente plan de financiaci6n, dado el monto total del vehículo, 
-         //el cliente debe pagar e1 35 %del enganche y el resto en 18 mensualidades iguales sin intereses. 
-         //Construya el diagrama de flujo que permita obtener cual es el importe del enganche y las mensualidades que debe pagar el cliente.
-            int montoTotal = 204000000;
-            int pagoEnganche = montoTotal * 0.35;
-            int mensualidad = (montoTotal - pagoEnganche) / 18;
-            printf("Monto Total: %d\nPago Enganche: %d\nMensualidad: %d",montoTotal, pagoEnganche,mensualidad);
+         //Tres personas deciden invertir su dinero para crear una empresa. cada una de ellas invierte una Cantidad distinta. 
+         //Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida.
+            double persona1 = 10000000;
+            double persona2 = 8000000;
+            double persona3 = 11000000;
+            double cantidadTotal = persona1 + persona2 + persona3;
+            double porcentaje1 = (100 * persona1) / cantidadTotal;
+            double porcentaje2 = (100 * persona2) / cantidadTotal;
+            double porcentaje3 = (100 * persona3) / cantidadTotal;
+            printf("Persona 1: %f\nPersona 2: %f\nPersona 3: %f", porcentaje1, porcentaje2, porcentaje3);
          }
