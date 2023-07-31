0ebabddd1cf178d19fd73e3da10c7802db3b69bb
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-31 18:38:42 -0500

    Ejercicio 18

--- Taller.c
+++ Taller.c
@@ -2,14 +2,12 @@
 #include<stdbool.h>
 #include<math.h>
 int main() {
-        //17.) Un vendedor recibe un sueldo base, más un bono extra por Comisión de sus ventas, el vendedor desea saber 
-        //cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes y el total 
-        //que recibirá en el mes tomando en cuenta su sueldo base y comisiones.
-            int sueldoBase = 1300000;
-            int comision1 = 15000;
-            int comision2 = 45000;
-            int comision3 = 25000;
-            int bonoExtraComision = comision1 + comision2 + comision3;
-            printf("Sueldo Base: %d\nComision: %d\nSueldo Total: %d", sueldoBase, bonoExtraComision,(bonoExtraComision + sueldoBase));
-            return 0;
+            //18.) En un hospital existen tres áreas: Ginecología, Pediatra, Traumatóloga.
+            //El presupuesto anual (o monto) del hospital se reparte conforme a la siguiente tabla:
+                double presupuestoAnual = 1000000000;
+                double ginecologia = presupuestoAnual * 0.40;
+                double pediatria = presupuestoAnual * 0.30;
+                double traumatologia = presupuestoAnual * 0.30;
+                printf("Presupuesto Anual: %.2f\nGinecologia: %.2f\nPediatria: %.2f\nTraumatologia: %.2f", 
+                presupuestoAnual, ginecologia, pediatria, traumatologia);
          }
